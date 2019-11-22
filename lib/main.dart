d77fd895a2451ed32e02d908cab628bcdb6c71a0
Cosmas Nyairo <nyairocosmas@gmail.com>
2019-11-22 19:48:44 +0300

    Transactions Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,15 +14,98 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 'a1',
+      title: 'Toothbrush',
+      amount: 200,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'a2',
+      title: 'Keyboard',
+      amount: 2000,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'a3',
+      title: 'Mouse',
+      amount: 1000,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'a4',
+      title: 'Watch',
+      amount: 1500,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground',),
-        ),
-      ); 
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+                color: Colors.deepOrange, elevation: 5, child: Text('Chart')),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 12,
+                      ),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Colors.teal,
+                        width: 2,
+                      )),
+                      child: Text(
+                        'Ksh:${tx.amount}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.teal),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
